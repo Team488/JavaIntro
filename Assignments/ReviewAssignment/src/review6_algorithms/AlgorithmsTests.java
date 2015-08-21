330037edc245871e591a07dd70eb5e8c27c6188a
WasabiFan <wasabifan@outlook.com>
2015-08-20 19:51:20 -0700

    Fix bubble sort tests

--- Assignments/ReviewAssignment/src/review6_algorithms/AlgorithmsTests.java
+++ Assignments/ReviewAssignment/src/review6_algorithms/AlgorithmsTests.java
@@ -11,8 +11,8 @@ public class AlgorithmsTests
     public void testBubbleSort()
     {
         assertArrayEquals("Bubble sort for array {1, 2, 3, 4, 5}", new int[] {5, 4, 3, 2, 1}, Algorithms.bubbleSort(new int[] {1, 2, 3, 4, 5}));
-        assertArrayEquals("Bubble sort for array {8, 2, 4, 10, 3, 6, 9}", new int[] {2, 3, 4, 6, 8, 9, 10}, Algorithms.bubbleSort(new int[] {8, 2, 4, 10, 3, 6, 9}));
-        assertArrayEquals("Bubble sort for array {8, 2, 4, 10, 3, 6, 9, 2}", new int[] {2, 2, 3, 4, 6, 8, 9, 10}, Algorithms.bubbleSort(new int[] {8, 2, 4, 10, 3, 6, 9, 2}));
+        assertArrayEquals("Bubble sort for array {8, 2, 4, 10, 3, 6, 9}", new int[] {10, 9, 8, 6, 4, 3, 2}, Algorithms.bubbleSort(new int[] {8, 2, 4, 10, 3, 6, 9}));
+        assertArrayEquals("Bubble sort for array {8, 2, 4, 10, 3, 6, 9, 2}", new int[] {10, 9, 8, 6, 4, 3, 2, 2}, Algorithms.bubbleSort(new int[] {8, 2, 4, 10, 3, 6, 9, 2}));
     }
 
 }
